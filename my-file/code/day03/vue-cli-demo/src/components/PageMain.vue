c8ef58ecfe6d330a7635837b550fcce654a3f826
Bangwu2001 <1985439923@qq.com>
2025-04-10 22:24:57 +0800

    03-vue组件化开发

---
+++ my-file/code/day03/vue-cli-demo/src/components/PageMain.vue
@@ -0,0 +1,26 @@
+<!--页面头部组件-->
+<template>
+  <div class="main">
+    页面主体
+    <PageButton></PageButton>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+  .main{
+    margin: 20px 0;
+    height: 300px;
+    background-color: blue;
+    text-align: center;
+    line-height: 300px;
+    font-size: 30px;
+    color: white;
+    font-weight: 700;
+  }
+</style>
