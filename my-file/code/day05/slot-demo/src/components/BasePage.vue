b1885910f48c2e14063e104fe203818fa5006e7e
BangW2001 <bangwu2001@gmail.com>
2025-04-27 16:01:24 +0800

    day05-all

---
+++ my-file/code/day05/slot-demo/src/components/BasePage.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <span><slot name="slot1">插槽1</slot></span>
+    <p><slot name="slot2">插槽2</slot></p>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
